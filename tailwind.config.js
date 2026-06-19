11b8c147131342dc852bcd7f58fc516564fb486b
Oxunjon Abdurashidov <oxunjonabdurashidov@gmail.com>
2026-06-19 07:56:21 +0500

    fix: remove type:commonjs, add npm scripts, extend primary color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#32a86f',
+        primary: {
+          DEFAULT: '#32a86f',
+          50: '#f0fdf6',
+          100: '#dcfce9',
+          200: '#bbf7d2',
+          300: '#86efad',
+          400: '#4ade80',
+          500: '#32a86f',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        },
       },
     },
   },
